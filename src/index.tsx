f93df4c380646550b9f14b32f026f8f3051ff495
Gabriela Paredes <gabiprds.93@gmail.com>
2024-05-25 14:02:27 -0500

    Add a framework class less Water.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './app';
+import React from "react"
+import ReactDOM from "react-dom"
 
-document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render((<App />), document.getElementById('reactMountPoint'));
-});
+import { App } from "./app"
+import "./global.css"
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<App />, document.getElementById("reactMountPoint"))
+})
