a7c2235bd98b00527075d5c1d34fb6aa18bb1ca8
Gabriela Paredes <gabiprds.93@gmail.com>
2024-05-25 15:27:08 -0500

    Replace code deprecaded

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
 import "./global.css"
 import App from "./app"
 
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<App />, document.getElementById("reactMountPoint"))
-})
+ReactDOM.createRoot(document.getElementById("reactMountPoint")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
